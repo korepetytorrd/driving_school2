1782246735c09ec928d70dceb3e7959af4245646
Andzrejek <jedrekg2000@gmail.com>
2025-01-15 16:45:46 +0100

    Update App.java
    Dodano Proszenie użytkownika o email i hasło , po czym wyświetla

--- src/App.java
+++ src/App.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
+
 public class App {
+  public static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
         String email;
         String haslo;
         //TODO Wczytanie danych email i hasło od użytkownika 
+        System.out.println("Podaj email");
+        email = sc.nextLine();
 
+        System.out.println("Podaj hasło");
+         haslo = sc.nextLine();
+
+        System.out.println("Twój email i hasło to:" + '\n' + email + '\n' + haslo);
+       
         
         
 
