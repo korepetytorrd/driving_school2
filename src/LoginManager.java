3408760f1645e0e31781ed2ed98cf6f1d6c7a992
Radek <korepetrycjerd@proton.me>
2025-01-23 20:16:39 +0100

    zrobiona metoda logowania oraz metoda do rozpoznawania kto jest zalogowany
    Co-authored-by: Andzrejek <Andzrejek@users.noreply.github.com>

--- src/LoginManager.java
+++ src/LoginManager.java
@@ -1,15 +1,31 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class LoginManager {
-
-    String email;
-    String pass;
-
-    public static com.example.app.User login(String email, String haslo){
-        FileManager.readUsersFromFile("user.txt");
-
-        return new com.example.app.User(email,haslo);
+    public static boolean  login(String email, String haslo, String path){
+        try (BufferedReader br = new BufferedReader(new FileReader(path))){
+            String line;
+            while ( (line =br.readLine()) != null ) {
+                String [] values = line.split(";");
+                if ( email.equals(values[3]) && haslo.equals(values[4]) ) return true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
-
+    public static int whoIsLogged(String email, String path){
+        try (BufferedReader br = new BufferedReader(new FileReader(path))){
+            String line;
+            while ( (line =br.readLine()) != null ) {
+                String [] values = line.split(";");
+                if ( email.equals(values[3]) ) return Integer.parseInt(values[7]);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,40 @@ import java.util.Scanner;
 public class Main {
     public static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
-        String email;
-        String haslo;
+        String email="rd@gmail.com";
+        String haslo="1234";
         //TODO Wczytanie danych email i hasło od użytkownika
         System.out.println("Podaj email");
-        email = sc.nextLine();
+        //email = sc.nextLine();
 
         System.out.println("Podaj hasło");
-        haslo = sc.nextLine();
+        //haslo = sc.nextLine();
 
-        System.out.println("Twój email i hasło to:" + '\n' + email + '\n' + haslo);
 
-        LoginManager.login(email, haslo);
+        if (LoginManager.login(email, haslo, "user.txt")){
+            System.out.println("Zalogowany");
+            switch (LoginManager.whoIsLogged(email, "user.txt")){
+
+                case 1:
+                    System.out.println("admin");
+                    break;
+                    case 2:
+                        System.out.println("instruktor");
+                        break;
+                        case 3:
+                            System.out.println("kursant");
+                            break;
+                            default:
+                                System.out.println("Nieznany User");
+                
+
+                
+            }
+
+        }else{
+            System.out.println("Niezalogowany");
+        }
+
 
 
 
