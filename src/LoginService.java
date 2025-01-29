ec01960d415eb5af7bb27c0b39de5c2130100e30
Andrzej <jedrekg2000@gmail.com>
2025-01-29 17:18:42 +0100

    Przeniesiono wszystko z klasy Main do nowej klasy LoginService.

---
+++ src/LoginService.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class LoginService {
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) throws Exception {
+        String email="rd@gmail.com";
+        String haslo="1234";
+        //TODO Wczytanie danych email i hasło od użytkownika
+        System.out.println("Podaj email");
+        email = sc.nextLine();
+
+        System.out.println("Podaj hasło");
+        haslo = sc.nextLine();
+
+
+        if (LoginManager.login(email, haslo, "user.txt")){
+            System.out.println("Zalogowany");
+            switch (LoginManager.whoIsLogged(email, "user.txt")){
+
+                case 1:
+                    System.out.println("admin");
+                    break;
+                    case 2:
+                        System.out.println("instruktor");
+                        break;
+                        case 3:
+                            System.out.println("kursant");
+                            break;
+                            default:
+                                System.out.println("Nieznany User");
+                
+
+                
+            }
+
+        }else{
+            System.out.println("Niezalogowany");
+        }
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,7 @@
-import java.util.Scanner;
 
 public class Main {
-    public static Scanner sc = new Scanner(System.in);
-    public static void main(String[] args) throws Exception {
-        String email="rd@gmail.com";
-        String haslo="1234";
-        //TODO Wczytanie danych email i hasło od użytkownika
-        System.out.println("Podaj email");
-        //email = sc.nextLine();
+    public static void main(String[] args){
 
-        System.out.println("Podaj hasło");
-        //haslo = sc.nextLine();
-
-
-        if (LoginManager.login(email, haslo, "user.txt")){
-            System.out.println("Zalogowany");
-            switch (LoginManager.whoIsLogged(email, "user.txt")){
-
-                case 1:
-                    System.out.println("admin");
-                    break;
-                    case 2:
-                        System.out.println("instruktor");
-                        break;
-                        case 3:
-                            System.out.println("kursant");
-                            break;
-                            default:
-                                System.out.println("Nieznany User");
-                
-
-                
-            }
-
-        }else{
-            System.out.println("Niezalogowany");
-        }
-
-
-
-
-
+        LoginService.main(email, haslo);
     }
-
-
 }
